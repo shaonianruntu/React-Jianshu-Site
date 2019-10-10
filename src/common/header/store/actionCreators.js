90092a829b68637241227cab2e7e8969bb62af4d
shaonianruntu <514574496@qq.com>
2019-10-10 19:43:36 +0800

    代码优化

--- src/common/header/store/actionCreators.js
+++ src/common/header/store/actionCreators.js
@@ -4,11 +4,17 @@
  * @Github:
  * @Date: 2019-10-10 15:30:10
  * @LastEditors: fangn
- * @LastEditTime: 2019-10-10 19:14:54
+ * @LastEditTime: 2019-10-10 19:26:46
  */
 import { actionTypes } from "./index";
 import { fromJS } from "immutable";
 import axios from "axios";
+
+const changeList = data => ({
+  type: actionTypes.CHANGE_LIST,
+  // 需要将 data 覆盖 reducer 里面原来的 list，但是原来的 list 是 immutable 的 list，如果直接使用从 ajax 获取到的普通的 list 来进行覆盖，会让原来的 list 也变成普通的 list，所以，需要使用 immutable 的 list 来覆盖原来的 list，反正 immutable 的特性被破坏。
+  data: fromJS(data)
+});
 
 export const getInputFocusAction = () => ({
   type: actionTypes.SEARCH_FOCUS
@@ -16,12 +22,6 @@ export const getInputFocusAction = () => ({
 
 export const getInputBlurAction = () => ({
   type: actionTypes.SEARCH_BLUR
-});
-
-const changeList = data => ({
-  type: actionTypes.CHANGE_LIST,
-  // 需要将 data 覆盖 reducer 里面原来的 list，但是原来的 list 是 immutable 的 list，如果直接使用从 ajax 获取到的普通的 list 来进行覆盖，会让原来的 list 也变成普通的 list，所以，需要使用 immutable 的 list 来覆盖原来的 list，反正 immutable 的特性被破坏。
-  data: fromJS(data)
 });
 
 export const getList = () => {
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -4,7 +4,7 @@
  * @Github:
  * @Date: 2019-10-10 15:21:39
  * @LastEditors: fangn
- * @LastEditTime: 2019-10-10 19:03:43
+ * @LastEditTime: 2019-10-10 19:41:51
  */
 import { actionTypes } from "./index";
 import { fromJS } from "immutable";
@@ -15,15 +15,15 @@ const defaultState = fromJS({
 });
 
 export default (state = defaultState, action) => {
-  if (action.type === actionTypes.SEARCH_FOCUS) {
-    // immutable 对象的 set 方法，会结合之前的 immutable 对象的值和设置的值，返回一个全新的对象
-    return state.set("focused", true);
+  switch (action.type) {
+    case actionTypes.SEARCH_FOCUS:
+      // immutable 对象的 set 方法，会结合之前的 immutable 对象的值和设置的值，返回一个全新的对象
+      return state.set("focused", true);
+    case actionTypes.SEARCH_BLUR:
+      return state.set("focused", false);
+    case actionTypes.CHANGE_LIST:
+      return state.set("list", action.data);
+    default:
+      return state;
   }
-  if (action.type === actionTypes.SEARCH_BLUR) {
-    return state.set("focused", false);
-  }
-  if (action.type === actionTypes.CHANGE_LIST) {
-    return state.set("list", action.data);
-  }
-  return state;
 };
